8a28ad0c56503267f4464f8463387ef92b7a2a74
DEEPIKA SHARMA <reverseclothingofficial@gmail.com>
2026-01-22 11:55:00 +0530

    tsc error fix

--- lib/mock/content.ts
+++ lib/mock/content.ts
@@ -1,12 +1,13 @@
-import type { Content } from "@promo/shared-types";
+import { Content } from "@/types/content";
 
 export const mockContents: Content[] = [
     {
-        id:"1",
+        _id:"1",
         title:"Winter Campaing banner",
         description:"Hello",
-        imageUrl:"https://plus.unsplash.com/premium_photo-1720744786849-a7412d24ffbf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmxvZ3xlbnwwfHwwfHx8MA%3D%3D",
-        videoUrl:"https://www.pexels.com/download/video/2435906/",
-        createdAt:"11-12-2025"
+        image:"https://plus.unsplash.com/premium_photo-1720744786849-a7412d24ffbf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmxvZ3xlbnwwfHwwfHx8MA%3D%3D",
+        video:"https://www.pexels.com/download/video/2435906/",
+        createdAt:"11-12-2025",
+        visibility:"public"
     }
 ]
